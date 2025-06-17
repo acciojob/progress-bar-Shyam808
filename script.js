ee7131a064cf1ef9ef2e04ac1c5ed6d5ddc521b4
Shyam Paghadal <shyampaghadal@gmail.com>
2025-06-17 13:51:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-//your JS code here. If required.
+const progress = document.getElementById("progress");
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+const circles = document.querySelectorAll(".circle");
+
+let currentStep = 1;
+
+next.addEventListener("click", () => {
+  currentStep++;
+  if (currentStep > circles.length) {
+    currentStep = circles.length;
+  }
+  update();
+});
+
+prev.addEventListener("click", () => {
+  currentStep--;
+  if (currentStep < 1) {
+    currentStep = 1;
+  }
+  update();
+});
+
+function update() {
+  circles.forEach((circle, idx) => {
+    if (idx < currentStep) {
+      circle.classList.add("active");
+    } else {
+      circle.classList.remove("active");
+    }
+  });
+
+  const activeCircles = document.querySelectorAll(".circle.active").length;
+  const totalSteps = circles.length - 1;
+
+  progress.style.width = `${(activeCircles - 1) / totalSteps * 100}%`;
+
+  prev.disabled = currentStep === 1;
+  next.disabled = currentStep === circles.length;
+}
